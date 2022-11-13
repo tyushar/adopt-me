5a466b6ea7a19bd1ea762daf2064732acb639677
Tushar Pandey <tushar0471@gmail.com>
2022-11-13 11:19:09 +0530

    updated App and Pet

--- src/App.js
+++ src/App.js
@@ -1,28 +1,16 @@
-import React from 'react';
-
-import ReactDOM from 'react';
+import { render } from 'react-dom';
 
 import Pet from './Pet';
 
 const App = () => {
-  return React.createElement('div', {}, [
-    React.createElement('h1', { id: 'my-brand' }, 'Adopt Me!'),
-    React.createElement(Pet, {
-      name: 'luna',
-      animal: 'Dog',
-      breed: 'Havanese',
-    }),
-    React.createElement(Pet, {
-      name: 'Pepper',
-      animal: 'Bird',
-      breed: 'Cockatiel',
-    }),
-    React.createElement(Pet, {
-      name: 'Sudo',
-      animal: 'Dog',
-      breed: 'Wheaten Terrier',
-    }),
-  ]);
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Luna" animal="Dog" breed="Havanese" />
+      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
+      <Pet name="Sudo" animal="Cat" breed="Wheaten" />
+    </div>
+  );
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
+render(<App />, document.getElementById('root'));
