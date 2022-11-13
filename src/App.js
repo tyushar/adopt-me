fc46ee6297eca4ededbe705ca53404dd64b8d788
Tushar Pandey <tushar0471@gmail.com>
2022-11-14 00:22:33 +0530

    useState hooks

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import { render } from 'react-dom';
 
-import Pet from './Pet';
+import SearchParams from './SearchParams';
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Luna" animal="Dog" breed="Havanese" />
-      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
-      <Pet name="Sudo" animal="Cat" breed="Wheaten" />
+      <SearchParams />
     </div>
   );
 };
